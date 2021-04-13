3dd525bc899275d3ea7cbdf00bca84e1b6d78183
Hamka Mannan <hamka@mannan.id>
2021-04-14 06:30:34 +0700

    Fixing

---
+++ src/Hello.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace hamkamannan\hello;
+
+class Hello{
+    public function sayHi($name){
+        echo "Hello $name welcome to PHP and Composer";
+    }
+}
--- src/HelloWorld.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace hamkamannan\hellolib;
-
-class HelloWorld{
-    public function sayHello($name){
-        echo "Hello $name welcome to PHP and Composer";
-    }
-}
